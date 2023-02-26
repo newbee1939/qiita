f2ff2fa38d6d9ede6cea96c26713e71663b633a3
newbee1939 <hideto3970@gmail.com>
2023-02-27 00:53:20 +0900

    仮実装

--- likesCountRanking.ts
+++ likesCountRanking.ts
@@ -1,1 +1,121 @@
-//
+import axios from "axios";
+import * as fs from "node:fs/promises";
+import dotenv from "dotenv";
+dotenv.config();
+
+const accessToken = process.env.ACCESS_TOKEN;
+
+makeLikesRankingArticle();
+
+async function makeLikesRankingArticle() {
+  const likesRanking = await makeLikesRanking();
+  await makeAndPostArticle(likesRanking);
+}
+
+async function makeLikesRanking() {
+  let pageNumber = 1;
+  let allResponseData = [];
+  while (true) {
+    const responseData = (
+      await axios.get("https://qiita.com//api/v2/items", {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        params: {
+          query: "stocks:>2000",
+          page: pageNumber,
+          per_page: 100,
+        },
+      })
+    ).data.map((article: any) => {
+      return {
+        title: article.title,
+        likesCount: article.likes_count,
+        createdAt: article.created_at,
+        updatedAt: article.updated_at,
+        url: article.url,
+      };
+    });
+
+    if (responseData.length === 0) {
+      break;
+    }
+
+    allResponseData.push(responseData);
+
+    pageNumber++;
+  }
+
+  const likesRanking = allResponseData
+    .flat()
+    .sort((a: any, b: any) => {
+      if (a.stocksCount > b.stocksCount) {
+        return -1;
+      }
+      if (a.stocksCount < b.stocksCount) {
+        return 1;
+      }
+      return 0;
+    })
+    .slice(0, 100);
+
+  return likesRanking;
+}
+
+// TODO:一回目投稿した後は更新にしたい
+// 一度投稿したらこれに変更で良さそう（https://qiita.com/api/v2/docs#patch-apiv2itemsitem_id）
+async function makeAndPostArticle(likesRanking: any) {
+  const articleInformation = {
+    title: "【保存版】Qiita歴代いいね数ランキング100",
+    body: await makeArticleBody(likesRanking),
+    private: true,
+    tags: [
+      { name: "TypeScript" },
+      { name: "QiitaAPI" },
+      { name: "Qiita" },
+      { name: "JavaScript" },
+      { name: "初心者" },
+    ],
+  };
+
+  try {
+    await axios.post("https://qiita.com/api/v2/items", articleInformation, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("投稿が完了しました！");
+  } catch (e) {
+    console.log(e);
+    console.log("投稿に失敗しました。。");
+  }
+}
+
+async function makeArticleBody(likesRanking: any) {
+  const lead = "※この記事は定期的に更新して最新状態を保ちます<br>";
+  const articleBody = likesRanking.reduce(
+    async (prevArticleBody: string, rankingData: any, index: number) => {
+      const content = await fs.readFile("likesRanking.md", "utf-8");
+      return (
+        (await prevArticleBody) +
+        content
+          .replace("rank", `${index + 1}`)
+          .replace("title", rankingData.title)
+          .replace("like", rankingData.likesCount)
+          .replace("url", rankingData.url)
+          .replace("createdAt", formatDate(rankingData.createdAt))
+          .replace("updatedAt", formatDate(rankingData.updatedAt))
+      );
+    },
+    lead
+  );
+
+  return articleBody;
+}
+
+export const formatDate = (dateTime: string): string => {
+  const date: Date = new Date(dateTime);
+
+  return `${date.getFullYear()}年${date.getMonth() + 1}月${date.getDate()}日`;
+};
